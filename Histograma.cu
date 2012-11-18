158458064d396b406f83c39f97938a6827e6666a
Jose <jmoc.14@gmail.com>
2012-11-18 01:00:17 -0300

    error de escritura
    Signed-off-by: Jose <jmoc.14@gmail.com>

--- Histograma.cu
+++ Histograma.cu
@@ -3,11 +3,11 @@
 #include <time.h>
 #include <cuda.h>
 
-#define HIST_LENGHT 256
+#define HIST_LENGTH 256
 #define NUMBER_OF_THREADS 512
 
 double time_diff(struct timeval x , struct timeval y);
-void CUDA_Hist(int *data_h, int *hist_h, int array_lenght);
+void CUDA_Hist(int *data_h, int *hist_h, int array_length);
 
 __global__ void GPUfuncion(int *hist, int *data, int max)
 {
@@ -16,11 +16,11 @@ __global__ void GPUfuncion(int *hist, int *data, int max)
 	int B = blockDim.x;
 	int value;
 
-	__shared__ int hist_temp[HIST_LENGHT];
+	__shared__ int hist_temp[HIST_LENGTH];
 
 	int index = b * B + t;
 	
-	if (t < HIST_LENGHT)
+	if (t < HIST_LENGTH)
 		hist_temp[t] = 0;
 
 	__syncthreads();
@@ -31,7 +31,7 @@ __global__ void GPUfuncion(int *hist, int *data, int max)
 		atomicAdd(&(hist_temp[value]), 1);
 		__syncthreads();
 
-		if (t < HIST_LENGHT)
+		if (t < HIST_LENGTH)
 			atomicAdd(&(hist[t]), hist_temp[t]);
 
 		return;
@@ -42,7 +42,7 @@ __global__ void GPUfuncion(int *hist, int *data, int max)
 
 int main()
 {	
-	int matrix_dim, array_lenght, *data_h, hist_h[HIST_LENGHT];
+	int matrix_dim, array_length, *data_h, hist_h[HIST_LENGTH];
 	int i;
 
 	/** Ficheros de entrada y salida **/
@@ -51,12 +51,12 @@ int main()
 	
 	/** Leer el primer numero que determina el tamano de la matriz **/
 	fscanf(in_f, "%d", &matrix_dim);
-	array_lenght = matrix_dim * matrix_dim;
+	array_length = matrix_dim * matrix_dim;
 
-	data_h = (int *)malloc(array_lenght * sizeof(int));
-	for (i = 0; i < array_lenght && fscanf(in_f, "%d", &data_h[i]) == 1; ++i);
+	data_h = (int *)malloc(array_length * sizeof(int));
+	for (i = 0; i < array_length && fscanf(in_f, "%d", &data_h[i]) == 1; ++i);
 
-	CUDA_Hist(data_h, hist_h, array_lenght);
+	CUDA_Hist(data_h, hist_h, array_length);
 
 	for (i = 0; i < 256; i++)
 	{
@@ -72,22 +72,22 @@ int main()
 	return 0;
 }
 
-void CUDA_Hist(int *data_h, int *hist_h, int array_lenght)
+void CUDA_Hist(int *data_h, int *hist_h, int array_length)
 {
 	int *data_d, *hist_d, blocks;
 	int block_size = NUMBER_OF_THREADS;
 
-	cudaMalloc((void **) &data_d, array_lenght * sizeof(int));
-	cudaMalloc((void **) &hist_d, HIST_LENGHT * sizeof(int));
+	cudaMalloc((void **) &data_d, array_length * sizeof(int));
+	cudaMalloc((void **) &hist_d, HIST_LENGTH * sizeof(int));
 
-	cudaMemcpy(data_d, data_h, array_lenght * sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemset(hist_d, 0, HIST_LENGHT * sizeof(int));
+	cudaMemcpy(data_d, data_h, array_length * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemset(hist_d, 0, HIST_LENGTH * sizeof(int));
 
-	blocks = ceil((float)array_lenght/block_size);
+	blocks = ceil((float)array_length/block_size);
 
-	GPUfuncion <<<blocks, block_size>>> (hist_d, data_d, array_lenght);
+	GPUfuncion <<<blocks, block_size>>> (hist_d, data_d, array_length);
 
-	cudaMemcpy(hist_h, hist_d, HIST_LENGHT * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(hist_h, hist_d, HIST_LENGTH * sizeof(int), cudaMemcpyDeviceToHost);
 
 	cudaFree(data_d);
 	cudaFree(hist_d);
