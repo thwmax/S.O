2a9337e57f95f95b91c0776fff36d306c1a3c851
Max Soderlund <mpsd92@gmail.com>
2012-11-20 09:31:45 -0300

    Comentarios histograma

--- Histograma/Histograma.c
+++ Histograma/Histograma.c
@@ -6,9 +6,8 @@ double time_diff(struct timeval x , struct timeval y);
 
 int main(int argc, char *argv[])
 {
-	/**Guarda hora de inicio**/
+	/** Declaracon de variables para medir el tiempo **/
 	struct timeval before , after;
-	
 	
 	int matrixsize, i, j, histogram[256], *numbers;
 	
@@ -19,23 +18,26 @@ int main(int argc, char *argv[])
 	/** Leer el primer numero que determina el tamano de la matriz **/
 	fscanf(in, "%d", &matrixsize);
 	
-	/**Llena el arreglo contador con ceros**/
-	for (i = 0; i < 256; i++)
-		histogram[i] = 0;
+	/** Llena el arreglo contador con ceros **/
+	histogram[] = {0};
 	
-	/**Asigna dinamicamente el tamano necesario para almacenar los enteros**/
+	/** Asigna dinamicamente el tamano necesario para almacenar los enteros **/
 	numbers = (int *)malloc(matrixsize * matrixsize * sizeof(int));
 	
-	/**Asigna todos los enteros a un arreglo**/
+	/** Asigna todos los enteros a un arreglo **/
 	for (i = 0; i < matrixsize * matrixsize && fscanf(in, "%d", &numbers[i]) == 1; ++i);
-
+	
+	/** Se da la partida al reloj **/
 	gettimeofday(&before , NULL);
-	/**Recorre el arreglo y compara cada numero con la histogram para sumar 1 al contador del numero calzado**/
+	
+	/** Recorre el arreglo y compara cada numero con la histogram para sumar 1 al contador del numero calzado **/
 	for (i = 0; i < matrixsize * matrixsize; i++)
 		histogram[numbers[i]]++;
+	
+	/** Parar el reloj **/
 	gettimeofday(&after , NULL);
 	
-	/**Escribe en el archivo out la cantidad de cada numero encontrado**/
+	/** Escribe en el archivo out la cantidad de cada numero encontrado **/
 	for (i = 0; i < 256; i++)
 	{
 		if (i == 255)
@@ -44,24 +46,23 @@ int main(int argc, char *argv[])
 			fprintf(out, "%d\n", histogram[i]);
 	}
 	
+	printf("Tiempo de ejecucion: %f [ms]\n" , time_diff(before , after) );
+	
 	fclose(in);
 	fclose(out);
-	
-	/**Parar el reloj**/
-	
-	printf("Tiempo de ejecucion: %lf [ms]\n" , time_diff(before , after) );
-
 	return 0;
 }
 
+/** Funcion que retorna una diferencia en milisegundos de dos variables, sacada de internet **/
+/** Link: http://www.binarytides.com/get-time-difference-in-microtime-in-c/ **/
 double time_diff(struct timeval x , struct timeval y)
 {
-		double x_ms , y_ms , diff;
-	   
-		x_ms = (double)x.tv_sec*(double)1000 + (double)x.tv_usec/(double)1000;
-		y_ms = (double)y.tv_sec*(double)1000 + (double)y.tv_usec/(double)1000;
-	   
-		diff = (double)y_ms - (double)x_ms;
-	   
-		return diff;
+	double x_ms , y_ms , diff;
+	
+	x_ms = (double)x.tv_sec*(double)1000 + (double)x.tv_usec/(double)1000;
+	y_ms = (double)y.tv_sec*(double)1000 + (double)y.tv_usec/(double)1000;
+	
+	diff = (double)y_ms - (double)x_ms;
+	
+	return diff;
 }
--- Histograma/Histograma.cu
+++ Histograma/Histograma.cu
@@ -8,37 +8,46 @@
 
 void CUDA_Hist(int *data_h, int *hist_h, int array_length);
 
+/** Funcion kernel de CUDA que procesa los datos **/
 __global__ void GPUfuncion(int *hist, int *data, int max)
 {
+	/** Declaracon de variables identificadoras de hebras **/
 	int i = blockDim.x * blockIdx.x + threadIdx.x;
 	int t = threadIdx.x;
-
-	__shared__ int local_hist[256];
-
-	if (t < 256)
+	
+	/** Declaracion de memoria compartida **/
+	__shared__ int local_hist[HIST_LENGTH];
+	
+	/** Inicializa el arreglo con puros 0**/
+	if (t < HIST_LENGTH)
 		local_hist[t] = 0;
-
+	
+	
+	/** Barrera para esperar a que todas las hebras hayan declarado el arreglo entero **/
 	__syncthreads();
-
+	
+	/** Si la hebra esta en el rango del histograma, se declara un auxiliar
+	    con la posicion y se suma 1 en el arreglo del histograma **/
 	if (i < max) {
 		int aux = data[i];
 		atomicAdd(&local_hist[aux], 1);
 	}
-
+	
+	/** Barrera para que todas las hebras hayan recorrido entero el arreglo del histograma **/
 	__syncthreads();
-
-	if (t < 256)
+	
+	/** Similar al anterior if pero ahora se le suma 1 a la suma ya calculada **/
+	if (t < HIST_LENGTH)
 		atomicAdd(&hist[t], local_hist[t]);
 }
 
 int main(int argc, char *argv[])
-{	
-	float elapsedTime;
+{
+	/** Declaracion de variables para medir el tiempo **/
 	cudaEvent_t start, stop;
-
-	int matrix_dim, array_length, *data_h, hist_h[HIST_LENGTH];
-	int i;
-
+	float elapsedTime;
+	int i, matrix_dim, array_length, *data_h, hist_h[HIST_LENGTH];
+	
 	/** Ficheros de entrada y salida **/
 	FILE *in_f = fopen(argv[1], "r");
 	FILE *out_f = fopen("salida", "w");
@@ -46,55 +55,69 @@ int main(int argc, char *argv[])
 	/** Leer el primer numero que determina el tamano de la matriz **/
 	fscanf(in_f, "%d", &matrix_dim);
 	array_length = matrix_dim * matrix_dim;
-
+	
+	/** Declaracion dinamica del tamano del arreglo dependiendo de la matriz **/
 	data_h = (int *)malloc(array_length * sizeof(int));
 	for (i = 0; i < array_length && fscanf(in_f, "%d", &data_h[i]) == 1; ++i);
-
+	
+	/** Se da inicio al reloj **/
 	cudaEventCreate(&start);
 	cudaEventCreate(&stop);
 	cudaEventRecord(start, 0);
-
+	
+	/** Llamado a la funcion de CUDA que procesa los datos **/
 	CUDA_Hist(data_h, hist_h, array_length);
-
+	
+	/** Se detiene el reloj **/
 	cudaEventRecord(stop, 0);
 	cudaEventSynchronize(stop);
+	
+	/** Se calcula la diferencia entre ambos tiempos **/
 	cudaEventElapsedTime(&elapsedTime, start, stop);
 	cudaEventDestroy(start);
-	cudaEventDestroy(stop);	
-
-	for (i = 0; i < 256; i++)
+	cudaEventDestroy(stop);
+	
+	/** Se imprime en el archivo **/
+	for (i = 0; i < HIST_LENGTH; i++)
 	{
-		if (i == 255)
+		if (i == HIST_LENGTH - 1)
 			fprintf(out_f, "%d", hist_h[i]);
 		else
 			fprintf(out_f, "%d\n", hist_h[i]);
 	}
 	
+	printf("Tiempo de ejecucion: %f [ms]\n", elapsedTime);
+	
 	fclose(in_f);
 	fclose(out_f);
-	
-	printf("Tiempo de ejecucion: %f [ms]\n", elapsedTime);
-
 	return 0;
 }
 
+/** Funcion que recibe el puntero al arreglo con los datos, otro al histograma
+    y el largo del arreglo, hace el copiado de memoria necesario y manda los datos
+    a la funcion kernel para su procesamiento **/
 void CUDA_Hist(int *data_h, int *hist_h, int array_length)
 {
 	int *data_d, *hist_d, blocks;
 	int block_size = NUMBER_OF_THREADS;
-
+	
+	/** Se reserva la memoria dinamicamente dependiendo el tamano de la matriz **/
 	cudaMalloc((void **) &data_d, array_length * sizeof(int));
 	cudaMalloc((void **) &hist_d, HIST_LENGTH * sizeof(int));
-
+	
+	/** Se copian los datos desde la memoria principal a la de la tarjeta grafica **/
 	cudaMemcpy(data_d, data_h, array_length * sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemset(hist_d, 0, HIST_LENGTH * sizeof(int));
 	
 	cudaMemcpy(hist_d, hist_h, HIST_LENGTH * sizeof(int), cudaMemcpyHostToDevice);
-
+	
+	/** Se definen la cantidad de bloques necesarios para el paralelismo optimo **/
 	blocks = (int)ceil(array_length/512.0);
-
-	GPUfuncion <<<blocks, block_size, 256 * sizeof(int)>>> (hist_d, data_d, array_length);
-
+	
+	/** Llamado a la funcion kernel que procesa los datos **/
+	GPUfuncion <<<blocks, block_size, HIST_LENGTH * sizeof(int)>>> (hist_d, data_d, array_length);
+	
+	/** Se libera la memoria solicitada **/
 	cudaFree(data_d);
 	cudaFree(hist_d);
 	return;
